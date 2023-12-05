a85772d18b4e8eaeb29baaccb7716d61194ce01a
hiranjanperera7532 <hiranjanperera7532@gmail.com>
2023-12-05 16:45:02 +0530

    initial commit

---
+++ src/main/java/com/issuestrackingsystem/issuestrackingsystem/IssuesTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.issuestrackingsystem.issuestrackingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssuesTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssuesTrackingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/issuestrackingsystem/issuestrackingsystem/IssuesTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.issuestrackingsystem.issuestrackingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssuesTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
